e10273553039dabaeea630cf804bf244c52012cb
Valeriy Zimnev <sorption.music@gmail.com>
2022-10-16 14:44:56 +0300

    Save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import classNames from 'classnames';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
+import { getRandomElementFromArray, getWhatcharData } from './logic';
+import { shuffle } from './logic/shuffle';
+
+const EMOJI_DIC = ['👌', '🤌', '🤟', '👍', '🙌', '👏'];
+const getRandomEmoji = () => getRandomElementFromArray(EMOJI_DIC);
+
+const VariantButton = ({
+  char,
+  value = " ",
+  onClick,
+  isAnswer = false,
+}) => {
+  const handleClick = useCallback(() => {
+    if (onClick) {
+      onClick(char);
+    }
+  }, [char, onClick]);
+
+  const activeClass = isAnswer ? "active:bg-green-500" : "active:bg-red-500";
+
+  return (
+    <button onClick={handleClick} className={`w-full outline-none justify-center items-center leading-4 text-center font-medium rounded-full text-white px-4 py-8 bg-white bg-opacity-5 ${activeClass} text-2xl overflow-hidden`}>
+      {value.toUpperCase()}
+    </button>
+  )
+}
 
 function App() {
+  const [success, setSuccess] = useState(false);
+  const [data, setData] = useState({
+    answer: {},
+    variants: [],
+  });
+
+  function calculate() {
+    setSuccess(false);
+    const { answer, variants } = getWhatcharData();
+    setData({
+      answer,
+      variants,
+    });
+  }
+
+  useEffect(() => {
+    calculate();
+  }, []);
+
+  const { answer, variants } = data || {};
+
+  const onVariantSelected = useCallback((char) => {
+    console.log('CLICKED', char);
+    if (answer.char === char.char) {
+      console.log('%c%s', 'font-weight:bold;color:green', 'YES!');
+      setTimeout(() => {
+        setSuccess(true);
+
+        setTimeout(() => {
+          calculate();
+        }, 500);
+  
+      }, 300);
+    } else {
+      console.log('%c%s', 'font-weight:bold;color:red', 'FUCK NO!');
+      window.navigator.vibrate(200);
+    }
+  }, [answer.char]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classNames(
+      "flex justify-center align-middle w-full h-full overflow-hidden",
+      success && "bg-green-400",
+    )}>
+      <div className="flex flex-grow max-w-[420px] px-4 flex-col justify-center items-center text-center">
+        <div className="mb-16">
+          <p className={classNames("mt-4 font-bold text-white",
+          success ? "bg-white rounded-full p-8 text-8xl" : "text-9xl")}>
+            {success ? getRandomEmoji() : (answer.char || "?")}
+          </p>
+        </div>
+        {!success && (
+          <p className="mb-6">
+            It's pronounced like:
+          </p>
+        )}
+        <div className="w-full grid grid-rows-2 grid-flow-col gap-4">
+          {!success && shuffle(shuffle(variants)).map(v => {
+            return (
+              <VariantButton
+                key={v.char}
+                char={v}
+                onClick={onVariantSelected}
+                value={v.spelling}
+                isAnswer={v.char === answer.char}
+              />
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/logic/index.js
@@ -0,0 +1,212 @@
+// const DICKTIONARY = [
+//     {
+//         char: "ア",
+//         spelling: "a"
+//     },
+//     {
+//         char: "イ",
+//         spelling: "i"
+//     },
+//     {
+//         char: "ウ",
+//         spelling: "u"
+//     },
+//     {
+//         char: "エ",
+//         spelling: "e"
+//     },
+//     {
+//         char: "オ",
+//         spelling: "o"
+//     },
+// ];
+
+const DICKTIONARY = [
+    {
+        char: "ა",
+        spelling: "а"
+    },
+    {
+        char: "ბ",
+        spelling: "б"
+    },
+    {
+        char: "გ",
+        spelling: "г"
+    },
+    {
+        char: "დ",
+        spelling: "д"
+    },
+    {
+        char: "ე",
+        spelling: "е"
+    },
+    {
+        char: "ვ",
+        spelling: "в"
+    },
+    {
+        char: "ზ",
+        spelling: "з"
+    },
+    {
+        char: "თ",
+        spelling: "тэ"
+    },
+    {
+        char: "ი",
+        spelling: "и"
+    },
+    {
+        char: "კ",
+        spelling: "кэ"
+    },
+    {
+        char: "ლ",
+        spelling: "л"
+    },
+
+    
+
+    {
+        char: "მ",
+        spelling: "м"
+    },
+    {
+        char: "ნ",
+        spelling: "н"
+    },
+    {
+        char: "ო",
+        spelling: "о"
+    },
+    {
+        char: "პ",
+        spelling: "п"
+    },
+    {
+        char: "ჟ",
+        spelling: "ж"
+    },
+    {
+        char: "რ",
+        spelling: "р"
+    },
+    {
+        char: "ს",
+        spelling: "с"
+    },
+    {
+        char: "ტ",
+        spelling: "т"
+    },
+    {
+        char: "უ",
+        spelling: "у"
+    },
+    {
+        char: "ფ",
+        spelling: "пэ"
+    },
+    {
+        char: "ქ",
+        spelling: "к"
+    },
+
+
+
+    {
+        char: "ღ",
+        spelling: "гэ"
+    },
+    {
+        char: "ყ",
+        spelling: "кʼ"
+    },
+    {
+        char: "შ",
+        spelling: "ш"
+    },
+    {
+        char: "ჩ",
+        spelling: "ч"
+    },
+    {
+        char: "ც",
+        spelling: "тс"
+    },
+    {
+        char: "ძ",
+        spelling: "дз"
+    },
+    {
+        char: "წ",
+        spelling: "ц"
+    },
+    {
+        char: "ჭ",
+        spelling: "чʼ"
+    },
+    {
+        char: "ხ",
+        spelling: "х"
+    },
+    {
+        char: "ჯ",
+        spelling: "джэ"
+    },
+    {
+        char: "ჰ",
+        spelling: "хʼ"
+    },
+];
+
+export function getRandomElementFromArray(arr = []) {
+    const len = arr.length;
+    const index = Math.round(Math.random() * (len - 1));
+    const result = arr[index] || {};
+    return result;
+}
+
+export function getWhatcharData() {
+    const answerChar = getRandomElementFromArray(DICKTIONARY);
+    const wrongChars = [];
+
+    let att = 0;
+    const ignoredChars = [answerChar.char];
+    const maxIgChars = 3;
+
+    while (true) {
+        if (att === maxIgChars) {
+            break;
+        }
+        let stop = false;
+        const _char = getRandomElementFromArray(DICKTIONARY);
+        
+        ignoredChars.forEach(e => {
+            if (_char.char === e) {
+                stop = true;
+            }
+        });
+
+        if (stop) {
+            continue;
+        }
+
+        att += 1;
+        ignoredChars.push(_char.char);
+        wrongChars.push(_char);
+    }
+
+    // console.log('ignoredChars', ignoredChars);
+    // console.log('---')
+    // console.log('Answer Char:', answerChar);
+    // console.log('Wrong Chars:', wrongChars);
+    return {
+        answer: answerChar,
+        wrong: wrongChars,
+        variants: [answerChar, ...wrongChars],
+    }
+}
+
